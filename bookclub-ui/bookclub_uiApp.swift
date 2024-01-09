04331780442c8519f10300baf594d972ff122d1e
TiannaLopes <tiannaalinalopes@gmail.com>
2024-01-09 09:19:13 -0500

    Adding swift project

---
+++ bookclub-ui/bookclub_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookclub_uiApp.swift
+//  bookclub-ui
+//
+//  Created by Tianna Alina Lopes on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct bookclub_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
