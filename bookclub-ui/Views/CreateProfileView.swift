14fb3fb31f8efc6b94fd38c4669df73df3eaded1
TiannaLopes <tiannaalinalopes@gmail.com>
2024-01-10 10:47:29 -0500

    Adding views scaffolding

---
+++ bookclub-ui/Views/CreateProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  CreateProfileView.swift
+//  bookclub-ui
+//
+//  Created by Tianna Alina Lopes on 1/10/24.
+//
+
+import Foundation
---
+++ bookclub-ui/Views/MyBookClubsView.swift
@@ -0,0 +1,8 @@
+//
+//  MyBookClubsView.swift
+//  bookclub-ui
+//
+//  Created by Tianna Alina Lopes on 1/10/24.
+//
+
+import Foundation
