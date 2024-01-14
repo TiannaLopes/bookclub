d40d4f739c8845ba8d7530a49b601b3cb58e03e7
TiannaLopes <tiannaalinalopes@gmail.com>
2024-01-14 12:14:44 -0500

    Adding show and hide password

--- bookclub-ui/Core/BookClubFeedView.swift
+++ bookclub-ui/Core/BookClubFeedView.swift
@@ -3,8 +3,8 @@ import SwiftUI
 
 struct BookClubFeedView: View {
     private let bookClubs =       [
-        BookClub(id: 1, name: "Morning Readers", description: "Join us for a lively morning discussion.", nextMeetingDate: Date().addingTimeInterval(86400 * 7)), // 7 days from now
-        BookClub(id: 2, name: "Sci-Fi Enthusiasts", description: "Exploring the universe, one book at a time.", nextMeetingDate: Date().addingTimeInterval(86400 * 14)), // 14 days from now
+        BookClub(id:" 1", name: "Morning Readers", description: "Join us for a lively morning discussion.", owner: "Snoop dawg", nextMeetingDate: Date().addingTimeInterval(86400 * 7)), // 7 days from now
+        BookClub(id: "2", name: "Sci-Fi Enthusiasts", description: "Exploring the universe, one book at a time.",owner: "Test", nextMeetingDate: Date().addingTimeInterval(86400 * 14)), // 14 days from now
         // More mock data...
     ]
 
--- bookclub-ui/Core/MyBookClubsView.swift
+++ bookclub-ui/Core/MyBookClubsView.swift
@@ -1,15 +1,19 @@
 import SwiftUI
 
 struct MyBookClubsView: View {
-    let myEvents: [BookClubEvent] = BookClubEvent.myEvents
-
     // Define the colors from your palette
     let darkMaroon = Color(hex: "3D0814")
     let paleYellow = Color(hex: "E7F9A9")
     let oliveGreen = Color(hex: "C9D3BE")
     let tan = Color(hex: "CDBB9E")
     let offWhite = Color(hex: "FDFDFD")
-
+    
+    private let myEvents =       [
+        BookClub(id:" 1", name: "Morning Readers", description: "Join us for a lively morning discussion.", owner: "Snoop dawg", nextMeetingDate: Date().addingTimeInterval(86400 * 7)), // 7 days from now
+        BookClub(id: "2", name: "Sci-Fi Enthusiasts", description: "Exploring the universe, one book at a time.",owner: "Test", nextMeetingDate: Date().addingTimeInterval(86400 * 14)), // 14 days from now
+        // More mock data...
+    ]
+    
     var body: some View {
         ZStack {
             // Background color
@@ -20,25 +24,18 @@ struct MyBookClubsView: View {
                     .font(.largeTitle)
                     .foregroundColor(darkMaroon)
                     .padding()
-
-                List {
-                    ForEach(myEvents.filter { $0.isAttending }) { event in
-                        VStack(alignment: .leading) {
-                            Text(event.bookClub.name)
-                                .font(.headline)
-                                .foregroundColor(darkMaroon)
-                            Text(event.bookClub.description)
-                                .font(.subheadline)
-                                .foregroundColor(darkMaroon)
-                            Text("Event Date: \(event.eventDate, formatter: itemFormatter)")
-                                .font(.caption)
-                                .foregroundColor(darkMaroon)                         }
-                        .padding()
-                        .background(offWhite)
-                        .cornerRadius(10)
-                        .shadow(radius: 5)
-                    }
-                }
+                
+                oliveGreen.edgesIgnoringSafeArea(.all)
+                      List {
+                          ForEach(myEvents) { bookClub in
+                              NavigationLink(destination: BookClubDetailView(bookClub: bookClub)) {
+                                  BookClubRowView(club: bookClub)
+                                      .padding()
+                                      .background(offWhite)
+                                      .cornerRadius(5.0)
+                              }
+                          }
+                      }
                 .listStyle(GroupedListStyle())
             }
         }
